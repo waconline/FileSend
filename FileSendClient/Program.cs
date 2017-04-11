af916ac62e8f9a547d7e3b4b2fdfcc7377b510b5
Никита <click.95@mail.ru>
2017-04-11 16:05:01 +0700

    first sync version

---
+++ FileSendClient/Program.cs
@@ -0,0 +1,46 @@
+﻿#region file header
+
+// Username []
+// Sln [FileSend]
+// Project [FileSendClient]
+// Filename [Program.cs]
+// Created  [11/04/2017 at 15:28]
+// Clean up [11/04/2017 at 15:48]
+// "we are circle 9. we are not retarded 
+//  what we lack in brains we have in brawn"
+
+#endregion
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace FileSendClient
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEndPoint = new IPEndPoint(ipAddress, 4423);
+            Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Hello,i am client");
+            try
+            {
+                sender.Connect(remoteEndPoint);
+                string toSendString = "";
+                while (!toSendString.Equals("exit"))
+                {
+                    toSendString = Console.ReadLine();
+                    sender.Send(Encoding.ASCII.GetBytes(toSendString));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
--- FileSendServer/Program.cs
+++ FileSendServer/Program.cs
@@ -1,15 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿#region file header
+
+// Username []
+// Sln [FileSend]
+// Project [FileSendServer]
+// Filename [Program.cs]
+// Created  [11/04/2017 at 15:28]
+// Clean up [11/04/2017 at 15:43]
+// "we are circle 9. we are not retarded 
+//  what we lack in brains we have in brawn"
+
+#endregion
+
+using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace FileSendServer
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+#pragma warning disable 618
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+#pragma warning restore 618
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 4423);
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Hello,i am server");
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+                
+                try
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Wait a connection");
+                        string data = "";
+                        Socket connectedClientSocket = listener.Accept();
+                        while (!data.Equals("exit"))
+                        {
+                            Console.WriteLine("Wait a message");
+                            byte[] bytes = new byte[1024];
+                            int bytesRec = connectedClientSocket.Receive(bytes);
+                            data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                            Console.WriteLine(data);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);                
+            }
         }
     }
 }
